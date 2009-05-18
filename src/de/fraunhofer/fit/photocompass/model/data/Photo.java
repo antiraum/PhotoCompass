92009496750c494d2a0db30605aa0db47ea6202b
Thomas Hess <thomas.hess@immer-weiter.de>
2009-05-18 08:48:40 +0200

    improved distance and direction methods

--- src/de/fraunhofer/fit/photocompass/model/data/Photo.java
+++ src/de/fraunhofer/fit/photocompass/model/data/Photo.java
@@ -1,53 +1,85 @@
 package de.fraunhofer.fit.photocompass.model.data;
 
+import android.location.Location;
+
 public class Photo {
 
 	private int _resourceId;
-	private float _lat;
-	private float _lng;
+	private double _lat;
+	private double _lng;
+	private float _distance;
+	private float _direction;
 	
 	public Photo(int resourceId, float lat, float lng) {
 		_resourceId = resourceId;
 		_lat = lat;
 		_lng = lng;
+		_distance = 0f;
+		_direction = 0f;
 	}
 
 	public int getResourceId() {
 		return _resourceId;
 	}
 
-	public float getLat() {
+	public double getLat() {
 		return _lat;
 	}
 
-	public float getLng() {
+	public double getLng() {
 		return _lng;
 	}
 	
 	/**
-	 * Returns distance of the photo from the current location.
+	 * Set the distance to a given position.
 	 * 
-	 * @param lat Latitude of the current location.
-	 * @param lng Longitude of the current location.
-	 * @return Distance in meters???
+	 * @param lat Latitude of the current location
+	 * @param lng Longitude of the current location
+	 * @return Distance in meters
 	 */
-	public double getDistance(float lat, float lng) {
-		// TODO calculate distance
-		double dist = Math.random();
-		if (dist < 0.3) dist = 0.3;
-		return dist;
+	public float updateDistance(double lat, double lng) {
+		
+		// calculate the distance
+		float[] results = new float[] {};
+		Location.distanceBetween(lat, lng, _lat, _lng, results);
+		_distance = results[0];
+		
+		// return result
+		return _distance;
 	}
 	
 	/**
-	 * Returns angle at which the photo lies from the current location.
+	 * Returns the saved distance
 	 * 
-	 * @param lat
-	 * @param lng
-	 * @return Angle (0 to 359). 0=North, 90=East, 180=South, 270=West
+	 * @return Distance in meters
 	 */
-	public float getAngle(float lat, float lng) {
-		// TODO calculate angle
-		double random = Math.random();
-		return (float)(random * 359);
+	public float getDistance() {
+		return _distance;
+	}
+	
+	/**
+	 * Set the direction to a given position.
+	 * 
+	 * @param lat Latitude of the current location
+	 * @param lng Longitude of the current location
+	 * @return Azimuth in degrees (0 - 360) 0 = North, 90 = East, 180 = South, 270 = West 
+	 */
+	public float updateDirection(double lat, double lng) {
+		
+		// TODO
+		
+		// dummy calculation
+		_direction = (float)(Math.random() * 359);
+		
+		return _direction;
+	}
+	
+	/**
+	 * Returns the saved direction
+	 * 
+	 * @return Azimuth in degrees
+	 */
+	public float getDirection() {
+		return _direction;
 	}
 }
