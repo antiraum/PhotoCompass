78f053ae1e66ce8c07b39517743feef09d509b74
joni <joni@port-192-168-76-70.dhcp.stw-bonn.de>
2009-05-28 01:42:03 +0200

    Horizontal + vertical DoubleSeekBar

--- src/de/fraunhofer/fit/photocompass/views/ControlsView.java
+++ src/de/fraunhofer/fit/photocompass/views/ControlsView.java
@@ -3,6 +3,7 @@ package de.fraunhofer.fit.photocompass.views;
 import android.content.Context;
 import android.widget.AbsoluteLayout;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import de.fraunhofer.fit.photocompass.R;
 
 public class ControlsView extends AbsoluteLayout {
@@ -10,14 +11,17 @@ public class ControlsView extends AbsoluteLayout {
 	public ControlsView(Context context) {
 		super(context);
 		
-        ImageView distanceSliderDummy = new ImageView(context);
-        distanceSliderDummy.setImageResource(R.drawable.distance_slider_dummy); 
-        distanceSliderDummy.setLayoutParams(new LayoutParams(62, 257, 6, 7));
-        addView(distanceSliderDummy);
+        DoubleSeekBar distanceSlider = new DoubleSeekBar(context, DoubleSeekBar.VERTICAL);
+        distanceSlider.setLayoutParams(new LayoutParams(62, 257, 6, 7));
+        addView(distanceSlider);
 
-        ImageView ageSliderDummy = new ImageView(context);
-        ageSliderDummy.setImageResource(R.drawable.age_slider_dummy); 
-        ageSliderDummy.setLayoutParams(new LayoutParams(441, 41, 32, 249));
-        addView(ageSliderDummy);
+//        SeekBar slider = new SeekBar(context);
+//        slider.setLayoutParams(new LayoutParams(441,29,32,200));
+//        this.addView(slider);
+//                
+        DoubleSeekBar ageSlider = new DoubleSeekBar(context, DoubleSeekBar.HORIZONTAL);
+//        ageSliderDummy.setImageResource(R.drawable.age_slider_dummy); 
+        ageSlider.setLayoutParams(new LayoutParams(441, 41, 32, 249));
+        addView(ageSlider);
     }
 }
