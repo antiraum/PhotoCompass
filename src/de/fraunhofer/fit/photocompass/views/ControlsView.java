ff486a83e4b80c09d33a46569468c787fcda726b
Thomas Hess <thomas.hess@immer-weiter.de>
2009-05-29 07:19:32 +0200

    replaced controls by dummies for today's presentation

--- src/de/fraunhofer/fit/photocompass/views/ControlsView.java
+++ src/de/fraunhofer/fit/photocompass/views/ControlsView.java
@@ -1,20 +1,33 @@
 package de.fraunhofer.fit.photocompass.views;
 
-import de.fraunhofer.fit.photocompass.views.controls.DoubleSeekBar;
 import android.content.Context;
 import android.widget.AbsoluteLayout;
+import android.widget.ImageView;
+import de.fraunhofer.fit.photocompass.R;
 
 public class ControlsView extends AbsoluteLayout {
 	
 	public ControlsView(Context context) {
 		super(context);
+
+        ImageView distanceSliderDummy = new ImageView(context);
+        distanceSliderDummy.setScaleType(ImageView.ScaleType.FIT_XY); 
+        distanceSliderDummy.setImageResource(R.drawable.distance_slider_dummy); 
+        distanceSliderDummy.setLayoutParams(new LayoutParams(62, 257, 6, 7));
+        addView(distanceSliderDummy);
 		
-        DoubleSeekBar distanceSlider = new DoubleSeekBar(context, DoubleSeekBar.VERTICAL);
-        distanceSlider.setLayoutParams(new LayoutParams(62, 257, 6, 7));
-        addView(distanceSlider);
+//        DoubleSeekBar distanceSlider = new DoubleSeekBar(context, DoubleSeekBar.VERTICAL);
+//        distanceSlider.setLayoutParams(new LayoutParams(62, 257, 6, 7));
+//        addView(distanceSlider);
+
+        ImageView ageSliderDummy = new ImageView(context);
+        ageSliderDummy.setScaleType(ImageView.ScaleType.FIT_XY); 
+        ageSliderDummy.setImageResource(R.drawable.age_slider_dummy); 
+        ageSliderDummy.setLayoutParams(new LayoutParams(441, 41, 32, 249));
+        addView(ageSliderDummy);
                 
-        DoubleSeekBar ageSlider = new DoubleSeekBar(context, DoubleSeekBar.HORIZONTAL);
-        ageSlider.setLayoutParams(new LayoutParams(441, 41, 32, 249));
-        addView(ageSlider);
+//        DoubleSeekBar ageSlider = new DoubleSeekBar(context, DoubleSeekBar.HORIZONTAL);
+//        ageSlider.setLayoutParams(new LayoutParams(441, 41, 32, 249));
+//        addView(ageSlider);
     }
 }
