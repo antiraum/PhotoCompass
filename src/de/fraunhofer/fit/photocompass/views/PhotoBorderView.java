c9e039140b1a8a67c3f10e50b195a86f7506ec7b
Thomas Hess <thomas.hess@immer-weiter.de>
2009-06-02 11:41:20 +0200

    added javadoc

--- src/de/fraunhofer/fit/photocompass/views/PhotoBorderView.java
+++ src/de/fraunhofer/fit/photocompass/views/PhotoBorderView.java
@@ -4,30 +4,53 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import de.fraunhofer.fit.photocompass.PhotoCompassApplication;
-import de.fraunhofer.fit.photocompass.model.ApplicationModel;
 
+/**
+ * This view is used by the {@link PhotosView} and displays the border for a photo.
+ * It's always used in conjunction with a {@link PhotoView} which displays the photo. The decoupling is needed cause
+ * photo borders should be visible even if the photo itself is occluded.
+ * The border is a thin {@link #PhotoCompassApplication.ORANGE} line than is less opaque the more photos occlude the 
+ * one this border belongs to.
+ */
 public class PhotoBorderView extends View {
 
+	private static final float BORDER_WIDTH = 3.1f; // stroke width of the border
+	private static final int ALPHA_DECREASE_PER_OCCLUSION = 15; // number by which the alpha value gets decreased for 
+															    // each photo occluding the one this border belongs to
+	
 	private int _width = 0;
 	private int _height = 0;
 	private Paint _paint;
 	
+	/**
+	 * Constructor.
+	 * Initializes the {@link #_paint}.
+	 * @param context
+	 */
 	public PhotoBorderView(Context context) {
 		super(context);
 		_paint = new Paint();
 		_paint.setColor(Color.parseColor(PhotoCompassApplication.ORANGE));
-		_paint.setStrokeWidth(3.1f);
+		_paint.setStrokeWidth(BORDER_WIDTH);
 	}
 	
+	/**
+	 * Set the number of photo occluding the one this border belongs to.
+	 * The more occlusions, the lower the alpha value of the {@link #_paint} is set.
+	 * @param numOcclusions Number of photos occluding the photo this border belongs to.
+	 */
 	public void setNumberOfOcclusions(int numOcclusions) {
-		int alpha = 255 - numOcclusions * 15;
+		int alpha = 255 - numOcclusions * ALPHA_DECREASE_PER_OCCLUSION;
 		_paint.setAlpha(alpha);
 	}
 	
+	/**
+	 * Called when the view should render its content.
+	 * All drawing is done here.
+	 */
     @Override 
     protected void onDraw(Canvas canvas) {
     	canvas.drawLine(0, 0, _width, 0, _paint); 
@@ -36,6 +59,10 @@ public class PhotoBorderView extends View {
     	canvas.drawLine(0, _height, 0, 0, _paint); 
     }
     
+    /**
+     * Called by the {@link PhotosView} when the view is repositioned or resized.
+     * We intercept to get the new dimensions.
+     */
     @Override
     public void setLayoutParams(ViewGroup.LayoutParams params) {
 		_width = params.width;
