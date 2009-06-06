6102cbaf6bb49ad081763b3d74678e2041a3a5a6
Thomas Hess <thomas.hess@immer-weiter.de>
2009-06-06 11:27:00 +0200

    removed obsolete import

--- src/de/fraunhofer/fit/photocompass/views/PhotoBorderView.java
+++ src/de/fraunhofer/fit/photocompass/views/PhotoBorderView.java
@@ -2,7 +2,6 @@ package de.fraunhofer.fit.photocompass.views;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 import android.view.ViewGroup;
