ec6f3327ccf8473d8b196c8bf98f0ae280eb11be
Thomas Hess <thomas.hess@immer-weiter.de>
2009-06-06 11:57:04 +0200

    fixed sequence bug in orientation service

--- src/de/fraunhofer/fit/photocompass/services/OrientationService.java
+++ src/de/fraunhofer/fit/photocompass/services/OrientationService.java
@@ -110,6 +110,7 @@ public final class OrientationService extends Service {
 			SensorManagerSimulator.connectSimulator(); 
 		} else {
 			// running on phone
+			_sensorManager = (SensorManager) getSystemService(Activity.SENSOR_SERVICE);
 
 			// check if there is no orientation sensor
 			if (_sensorManager.getSensorList(SensorManager.SENSOR_ORIENTATION).size() == 0) {
@@ -117,8 +118,6 @@ public final class OrientationService extends Service {
 	        	// TODO notify the user and tell him that he cannot use the application
 	    		return;
 	    	}
-			
-			_sensorManager = (SensorManager) getSystemService(Activity.SENSOR_SERVICE);
 		}
     	
     	// start listening to sensors
