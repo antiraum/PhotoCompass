7b7ea5cf3bd0a18da7695b1c652d9e04108e69a6
Thomas Hess <thomas.hess@immer-weiter.de>
2009-07-10 04:41:28 +0200

    increased update interval

--- src/de/fraunhofer/fit/photocompass/activities/FinderActivity.java
+++ src/de/fraunhofer/fit/photocompass/activities/FinderActivity.java
@@ -51,8 +51,8 @@ public final class FinderActivity extends Activity {
     private PhotosView _photosView;
     private long _lastCompassViewUpdate;
 	private long _lastPhotoViewUpdate;
-	private static final int COMPASS_VIEW_UPDATE_IVAL = 100; // in milliseconds
-	private static final int PHOTO_VIEW_UPDATE_IVAL = 250; // in milliseconds
+	private static final int COMPASS_VIEW_UPDATE_IVAL = 350; // in milliseconds
+	private static final int PHOTO_VIEW_UPDATE_IVAL = 350; // in milliseconds
 
     ILocationService locationService = null; // package scoped for faster access by inner classes
     private boolean _boundToLocationService = false;
